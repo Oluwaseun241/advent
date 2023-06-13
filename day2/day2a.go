eefdf0df8902b0dec3c7a048315279f9b4ba363e
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-06-13 15:09:32 +0100

    day2a

--- day2/day2a.go
+++ day2/day2a.go
@@ -10,29 +10,58 @@ import (
 // B : Paper
 // C : Scissors
 
-func strategy(input map[string]string) int {
-  score := 0
-  for _, response := range input {
-    if response == "Y" {
-      score += 8
-    } else if response == "X" {
-      score += 1
-    } else if response == "Z" {
-      score += 6
-    }
-  }
-  return score
-}
+
 
 func main() {
-  input,_ := os.Open("input.txt")
-  defer input.Close()
-  sc := bufio.NewScanner(input)
-
-  for sc.Scan() {
-    score := sc.Text()
+  f,_ := os.Open("input.txt")
+  defer f.Close()
+  sc := bufio.NewScanner(f)
+  
+  scores := map[string]int {
+    "A Y":8,
+    "A X":4,
+    "A Z":3,
+    "B X":1,
+    "B Y":5,
+    "B Z":9,
+    "C Z":6,
+    "C Y":2,
+    "C X":7,
   }
 
-  totalScore := strategy(input)
-  fmt.Println(totalScore)
+  var score int
+
+  for sc.Scan() {
+    score += scores[sc.Text()]
+  }
+  fmt.Println(score)
 }
+
+// SOlUTION TO THE EXAMPLE INPUT
+
+// func strategy(input map[string]string) int {
+//   score := 0
+//   for _, response := range input {
+//     if response == "Y" {
+//       score += 8
+//     } else if response == "X" {
+//       score += 1
+//     } else if response == "Z" {
+//       score += 6
+//     }
+//   }
+//   return score
+// }
+
+// func main() {
+// input := map[string]string {
+//      "A": "Y",
+//      "B": "X",
+//      "C": "Z",
+//   }
+//   fmt.Println(input)
+//
+//   totalScore := strategy(input)
+//   fmt.Println(totalScore)
+// }
+
