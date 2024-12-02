6ea67f07f37f55617e39c245731fe5a00ac3e8c5
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-12-02 09:32:48 +0100

    day1b

---
+++ day1/day1b.scala
@@ -0,0 +1,36 @@
+import scala.io.Source
+// start from the smallest
+// check how many times the left hand side appears on the right
+// multiply left hand side by number of occurence and store it
+//do this for all the list
+// add the total similarity score
+@main def main(): Unit = {
+  // example
+  // val input = List(
+  //   (3,4),
+  //   (4,3),
+  //   (2,5),
+  //   (1,3),
+  //   (3,9),
+  //   (3,3)
+  // )
+
+  // read txt file
+  val filename = "./input.txt"
+  val input = Source.fromFile(filename).getLines().toList.map { line =>
+    val parts = line.split("\\s+").map(_.trim.toInt)
+    (parts(0), parts(1))
+  }
+
+  var lhSide = input.map(_._1).sorted
+  var rhSide = input.map(_._2).sorted
+
+  // calculate similarity score
+  val similarity = lhSide.map { left  =>
+    val count = rhSide.count(_ == left)
+    
+    left * count
+  }.sum
+
+  println(s"Similarity score: $similarity")
+}
